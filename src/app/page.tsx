e0cadbd180af87ecc07a9858d1e88be8079ae4e0
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-07-30 00:25:55 +0700

    Color constans + Font Family set up [KienNDT]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button';
+import Colors from '@/constants/color';
 import Image from 'next/image';
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Button>Test button</Button>
+      <span className='text-primaryLight font-bold'>Hello world</span>
+    </div>
+  );
 }
