92fc80f55a649504c271b8e220dfedc3cf343cb7
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-08-15 21:40:15 +0700

    Responsive section five [KienNDT]

--- src/components/home/SectionFive.tsx
+++ src/components/home/SectionFive.tsx
@@ -4,22 +4,25 @@ import { Button } from '../ui/button';
 
 const SectionFive = () => {
   return (
-    <section className='flex flex-col items-center space-y-8'>
+    <section className='flex flex-col items-center space-y-6 px-4 md:px-0 text-center'>
       <h1 className='text-2xl font-semibold text-primaryLight'>
-        No spam promise
+        Không gửi thư rác
       </h1>
-      <h1 className='text-3xl font-semibold '>Are you a landlord?</h1>
-      <span className='text-slate-px-400'>
-        Discover ways to increase your home&apos;s value and get listed. No
-        Spam.
+      <h1 className='text-3xl font-semibold'>Bạn là chủ nhà?</h1>
+      <span className='text-slate-400 max-w-md'>
+        Khám phá các cách để tăng giá trị ngôi nhà của bạn và được niêm yết.
+        Không gửi thư rác.
       </span>
-      <div className='flex items-center gap-3'>
-        <Input placeholder='Enter your email here' className='w-[400px]' />
-        <Button>Submit</Button>
+      <div className='flex flex-col md:flex-row items-center gap-3 w-full md:w-auto'>
+        <Input
+          placeholder='Nhập email của bạn tại đây'
+          className='w-full md:w-[400px]'
+        />
+        <Button className='w-full md:w-auto'>Gửi</Button>
       </div>
-      <span>
-        Join <span className='text-primaryLight '>10,000+</span> other landlords
-        in our estatery community.
+      <span className='text-slate-500'>
+        Tham gia cùng <span className='text-primaryLight'>10,000+</span> chủ nhà
+        khác trong cộng đồng của chúng tôi.
       </span>
     </section>
   );
